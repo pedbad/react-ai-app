58057a44c5c33a7a99560f025d6bbe1d5b0f0138
pedbad <pb357@cam.ac.uk>
2025-11-15 23:48:57 +0000

    added express server script and bun run start script command in server package.json

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import Express from "express";
+import type { Request, Response } from "express";
+
+
+const app = Express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello from the server!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
