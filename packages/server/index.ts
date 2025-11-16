86c05cb98227bd150fb5b8760250f997c9f991fc
pedbad <pb357@cam.ac.uk>
2025-11-16 01:57:58 +0000

    connected front end client to backend server in vite.config.ts using server proxy setting

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -12,6 +12,10 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World!");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+    res.json({ message: "Hello from the API!"});
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
