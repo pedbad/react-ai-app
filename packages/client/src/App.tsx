8278eace6c08f0cd5e055c1283b62406c0bc8feb
pedbad <pb357@cam.ac.uk>
2025-11-16 13:53:59 +0000

    setup prettier and .prettierignore, added format command to package.json scripts in root directoty.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,25 +1,22 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 import { Button } from './components/ui/button';
 
-
 function App() {
-  const [message, setMessage] = useState('');
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error fetching message:', error));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.json())
+         .then((data) => setMessage(data.message))
+         .catch((error) => console.error('Error fetching message:', error));
+   }, []);
 
-  return (
-
-    <div className="p-4">
-      <p className="font-bold text-3xl">{message}</p>
-      <Button>Click Me</Button>
-    </div>
-
-  ); 
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message}</p>
+         <Button>Click Me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,21 +1,20 @@
-import Express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import Express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
-
 
 const app = Express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, World!");
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello, World!');
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-    res.json({ message: "Hello from the API!"});
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello from the API!' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+   console.log(`Server is running on http://localhost:${PORT}`);
 });
